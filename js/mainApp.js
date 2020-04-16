0ebd92e56d6946b7293cecc4b3bd7d3a547539e9
vijaykugithub <amit_90043@yahoo.in>
2020-04-16 20:58:27 +0530

    js file changed

--- js/mainApp.js
+++ js/mainApp.js
@@ -2,17 +2,17 @@ var app= angular.module("opportunityApp",[]);
 
 app.controller("OpportunityCtrl", function($scope,$http) {
     
- $scope.opportunityListData=opportunityListData;
+ //$scope.opportunityListData=opportunityListData;
     
-    // $http({
-    //     method: 'GET',      
-    //     url: 'https://reqres.in/api/users?page=2'            
-    //     }).then(function success(response) {
-    //     $scope.opportunityListData = response.data;     
-    //     $scope.statusval = response.status;       
-    //     $scope.statustext = response.statusText;      
-    //     $scope.headers = response.headers();      
-    //     }, function error(response) { 
-    //         console.log("some thing happen on the server.");       
-    //     });
+    $http({
+        method: 'GET',      
+        url: 'http://localhost:61350/api/Opportunity/GetCovidData'            
+        }).then(function success(response) {
+        $scope.covidData = response.data;     
+        $scope.statusval = response.status;       
+        $scope.statustext = response.statusText;      
+        $scope.headers = response.headers();      
+        }, function error(response) { 
+            console.log("some thing happen on the server.");       
+        });
 });
