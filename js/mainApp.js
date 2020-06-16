073ab60c46bd3b0b9f10af78c91e9c1959ffdd39
vijaykugithub <amit_90043@yahoo.in>
2020-06-17 00:24:18 +0530

    Update mainApp.js

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,4 +1,5 @@
 var urlA="https://covid19.mathdro.id/api";
+var urlB="https://covid19.mathdro.id/api/countries/";
 var app= angular.module("opportunityApp",[]);
 
 app.controller("OpportunityCtrl", function($scope,$http) {
@@ -25,7 +26,10 @@ app.controller("OpportunityCtrl", function($scope,$http) {
             $scope.c_data=undefined;
             return;
         }
-        $http.get('${urlA}/countries/${country}')
+        $http({
+        method: 'GET',      
+        url:  urlB+country           
+        })
         .then(function success(response) {
         $scope.c_data = response.data;          
         }, function error(response) { 
